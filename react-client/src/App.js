fe80577ce3dad69b76282d6516c9126495dfd822
Coder-Kruti <Nargund.kruti@gmail.com>
2023-08-17 00:04:07 +0300

    React app initial commit

---
+++ react-client/src/App.js
@@ -0,0 +1,14 @@
+import MyRouter from "./router";
+import NavigationBar from "./components/NavigationBar";
+
+function App() {
+  return (
+    <div>
+
+      <NavigationBar />
+      <MyRouter />
+    </div>
+  );
+}
+
+export default App;
